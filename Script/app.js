a9965347334ba91a144f145914a96fddf6f82571
Mydiam <pgm.mehdirabani@gmail.com>
2024-01-18 23:03:01 +0330

    Responsive Swiper Slider

--- Script/app.js
+++ Script/app.js
@@ -5,8 +5,28 @@ navToggle.addEventListener("click", function () {
   menuMobile.classList.toggle("menu--open");
 });
 const swiper = new Swiper(".swiper", {
+  slidesPerView: 3,
+  spaceBetween: 110,
   loop: true,
   pagination: {
     el: ".swiper-pagination",
+  },
+  breakpoints: {
+    576: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 20,
+    },
+    992: {
+      slidesPerView: 4,
+      spaceBetween: 30,
+    },
+    1400: {
+      slidesPerView: 4,
+      spaceBetween: 35,
+    },
   },
 });
