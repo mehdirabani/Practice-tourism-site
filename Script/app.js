c66b0731281b5bca81cb405a2a9f56bc56d03635
Mydiam <pgm.mehdirabani@gmail.com>
2024-01-18 22:07:18 +0330

    Import Local  Swiper Slider File

--- Script/app.js
+++ Script/app.js
@@ -1,7 +1,6 @@
 const navToggle = document.querySelector(".nav__toggle");
 const menuMobile = document.querySelector(".menu");
-
 navToggle.addEventListener("click", function () {
   navToggle.classList.toggle("nav__toggle--open");
-  menuMobile.classList.toggle("menu--open")
+  menuMobile.classList.toggle("menu--open");
 });
